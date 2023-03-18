5927c5ca7b90b9f19411dbd6a914503e9e8eb9b9
Joseph Abbey <joseph@abbey1.org.uk>
2023-03-18 19:02:48 +0000

    use environment variable for apollo uri

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 export default new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: process.env.URI + "/api/graphql",
   cache: new InMemoryCache()
 })
